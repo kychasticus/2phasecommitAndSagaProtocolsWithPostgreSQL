bc99427bbba45eaab14e809be06ddbd7d1eea9ba
kychasticus <ivan.kychatyj@gmail.com>
2020-03-14 15:46:12 +0100

    added commit + rollback

--- 2pcTransactionManager.py
+++ 2pcTransactionManager.py
@@ -1,6 +1,7 @@
 import psycopg2
 
 
+# PREPARATION FUNCTIONS
 def kick_connection(db_name):
 
     try:
@@ -17,20 +18,6 @@ def kick_connection(db_name):
     except psycopg2.Error as connection_error:
         print('The error occured during the connection:', connection_error)
 
-
-def execute_query_post_NoAutocommit(connection, query, data):
-
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute(query, data)
-            response = cursor.rowcount
-
-            return response
-
-        except psycopg2.Error as cursor_error:
-            print('The error occured while executing the query:', cursor_error)
-
-            return cursor_error
 
 def execute_query_post_autocommitrollback(connection, query):
 
@@ -64,6 +51,7 @@ def execute_query_pull(connection, query):
         return request_columns, request_output
 
 
+# INITIALIZE TABLES AND BALANCES FUNCTIONS
 def init_table(db_name, table_name, table_query):
     conn = kick_connection(db_name)
 
@@ -95,10 +83,10 @@ def init_account_balance():
     conn.close()
 
 
-def prepare_flight_transaction():
+# PREPARE TRANSACTION BLOCK
+def prepare_flight_transaction(transaction_name, transaction_details):
 
     db_name = 'FlyBooking'
-
     conn = kick_connection(db_name)
 
     query = """
@@ -106,87 +94,193 @@ def prepare_flight_transaction():
     (booking_id, client_name, fly_number, from_air, to_air, date) 
     VALUES(%s, %s, %s, %s, %s, %s);
     """
-    query_data = ('345235', 'peter', 'UA3456', 'KBP', 'TGL', '2020-02-20')
 
-    begin_response = execute_query_post_NoAutocommit(conn, 'BEGIN')
-    transaction_response = execute_query_post_NoAutocommit(conn, query, query_data)
-    prepare_response = execute_query_post_NoAutocommit(conn, "PREPARE TRANSACTION %s", 'peter_flight')
+    query_data = (f'{transaction_details[0]}',
+                  f'{transaction_details[1]}',
+                  f'{transaction_details[2]}',
+                  f'{transaction_details[3]}',
+                  f'{transaction_details[4]}',
+                  f'{transaction_details[5]}')
 
-    conn.close()
+    with conn.cursor() as cursor:
+        try:
+            responses = []
 
-    return begin_response, transaction_response, prepare_response
+            cursor.execute('BEGIN')
+            responses.append(cursor.rowcount)
+
+            cursor.execute(query, query_data)
+            responses.append(cursor.rowcount)
+
+            cursor.execute("PREPARE TRANSACTION %s", transaction_name)
+            responses.append(cursor.rowcount)
+
+            return responses
+
+        except psycopg2.Error as cursor_error:
+            print('The error occured while executing the query:', cursor_error)
+            conn.rollback()
+
+            return cursor_error
+
+        finally:
+            conn.close()
 
 
-def prepare_hotel_transaction():
+def prepare_hotel_transaction(transaction_name, transaction_details):
 
     db_name = 'HotelBooking'
-
     conn = kick_connection(db_name)
 
     query = f"""
     INSERT INTO public.htl_booking
     (booking_id, client_name, hotel_name, arrival, departure) 
-    VALUES('457634', 'peter', 'Ritz', '2020-02-20', '2020-03-20');
+    VALUES(%s, %s, %s, %s, %s);
     """
 
-    begin_response = execute_query_post_NoAutocommit(conn, 'BEGIN')
-    transaction_response = execute_query_post_NoAutocommit(conn, query)
-    prepare_response = execute_query_post_NoAutocommit(conn, "PREPARE TRANSACTION 'peter_hotel'")
+    query_data = (f'{transaction_details[0]}',
+                  f'{transaction_details[1]}',
+                  f'{transaction_details[2]}',
+                  f'{transaction_details[3]}',
+                  f'{transaction_details[4]}')
 
-    conn.close()
+    with conn.cursor() as cursor:
+        try:
+            responses = []
 
-    return begin_response, transaction_response, prepare_response
+            cursor.execute('BEGIN')
+            responses.append(cursor.rowcount)
+
+            cursor.execute(query, query_data)
+            responses.append(cursor.rowcount)
+
+            cursor.execute("PREPARE TRANSACTION %s", transaction_name)
+            responses.append(cursor.rowcount)
+
+            return responses
+
+        except psycopg2.Error as cursor_error:
+            print('The error occured while executing the query:', cursor_error)
+            conn.rollback()
+
+            return cursor_error
+
+        finally:
+            conn.close()
 
 
-def prepare_account_transaction():
+def prepare_account_transaction(transaction_name, transaction_details):
 
     db_name = 'Account'
-
     conn = kick_connection(db_name)
 
     query = f"""
     UPDATE public.account
-    SET amount = amount - 100 
-    WHERE client_name = 'peter';
+    SET amount = amount - %s 
+    WHERE account_id = %s;
     """
+    query_data = (f'{transaction_details[2]}',
+                  f'{transaction_details[0]}')
 
-    begin_response = execute_query_post_NoAutocommit(conn, 'BEGIN %s', '')
-    transaction_response = execute_query_post_NoAutocommit(conn, query)
-    prepare_response = execute_query_post_NoAutocommit(conn, "PREPARE TRANSACTION 'peter_account'")
+    with conn.cursor() as cursor:
+        try:
+            responses = []
 
-    conn.close()
+            cursor.execute('BEGIN')
+            responses.append(cursor.rowcount)
 
-    return begin_response, transaction_response, prepare_response
+            cursor.execute(query, query_data)
+            responses.append(cursor.rowcount)
+
+            cursor.execute("PREPARE TRANSACTION %s", transaction_name)
+            responses.append(cursor.rowcount)
+
+            return responses
+
+        except psycopg2.Error as cursor_error:
+            print('The error occured while executing the query:', cursor_error)
+            conn.rollback()
+
+            return cursor_error
+
+        finally:
+            conn.close()
 
 
-def commit_prepared_transaction(flight_transaction, hotel_transaction, account_transaction):
+# COMMIT OR ROLLBACK PREPARED BLOCK
+def finish_prepared_flight(flight_transaction, action):
 
     db_fly = 'FlyBooking'
-    db_hotel = 'HotelBooking'
-    db_account = 'Account'
-
-    fly_query = "COMMIT PREPARED %s"
-    fly_query_data = (flight_transaction,)
-    hotel_query = "COMMIT PREPARED %s"
-    hotel_query_data = (hotel_transaction,)
-    account_query = "COMMIT PREPARED %s"
-    account_query_data = (account_transaction,)
-
+    query = "COMMIT PREPARED %s" if action == 'commit' else "ROLLBACK PREPARED %s"
+    responses = []
     conn = kick_connection(db_fly)
-    fly_resp = execute_query_post_NoAutocommit(conn, fly_query, fly_query_data)
-    conn.close()
 
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute(query, (flight_transaction,))
+            responses.append(cursor.rowcount)
+
+            return responses
+
+        except psycopg2.Error as cursor_error:
+            print('The error occured while executing COMMIT PREPARED:', cursor_error)
+            conn.rollback()
+
+            return cursor_error
+
+        finally:
+            conn.close()
+
+
+def finish_prepared_hotel(hotel_transaction, action):
+
+    db_hotel = 'HotelBooking'
+    query = "COMMIT PREPARED %s" if action == 'commit' else "ROLLBACK PREPARED %s"
+    responses = []
     conn = kick_connection(db_hotel)
-    hotel_resp = execute_query_post_NoAutocommit(conn, hotel_query, hotel_query_data)
-    conn.close()
 
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute(query, (hotel_transaction,))
+            responses.append(cursor.rowcount)
+
+            return responses
+
+        except psycopg2.Error as cursor_error:
+            print('The error occured while executing the query:', cursor_error)
+            conn.rollback()
+
+            return cursor_error
+
+        finally:
+            conn.close()
+
+
+def finish_prepared_account(account_transaction, action):
+
+    db_account = 'Account'
+    query = "COMMIT PREPARED %s" if action == 'commit' else "ROLLBACK PREPARED %s"
+    responses = []
     conn = kick_connection(db_account)
-    account_resp = execute_query_post_NoAutocommit(conn, account_query, account_query_data)
-    conn.close()
 
-    return fly_resp, hotel_resp, account_resp
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute(query, (account_transaction,))
+            responses.append(cursor.rowcount)
+
+            return responses
+
+        except psycopg2.Error as cursor_error:
+            print('The error occured while executing the query:', cursor_error)
+            conn.rollback()
+
+            return cursor_error
+
+        finally:
+            conn.close()
 
 
+# INITIALIZE TABLES QUERIES
 CreateFlyBookTableQuery = """
 CREATE TABLE public.fly_booking (
 booking_id integer PRIMARY KEY,
@@ -214,8 +308,12 @@ amount numeric(20,2) CHECK(Amount >= 0)
 );
 """
 
-Record1 = {'flight': ['345235', 'peter', 'UA3456', 'KBP', 'TGL', '2020-02-20']}
+# TRANSACTIONS TO PERFORM
+Record1 = {'flight': ['345235', 'peter', 'UA3456', 'KBP', 'TGL', '2020-02-20'],
+           'hotel': ['457634', 'peter', 'Ritz', '2020-02-20', '2020-03-20'],
+           'account': ['456754', 'peter', '100']}
 
+# TRANSACTION MANAGER CODE ITSELF
 
 # Initialize tables at the distinct DB's
 init_table('FlyBooking', 'fly_booking', CreateFlyBookTableQuery)
@@ -224,13 +322,22 @@ init_table('Account', 'account', CreateAccountTableQuery)
 init_account_balance()
 
 # Prepare transactions
-f1, f2, f3 = prepare_flight_transaction()
-h1, h2, h3 = prepare_hotel_transaction()
-a1, a2, a3 = prepare_account_transaction()
+prepare_flight_response = prepare_flight_transaction('peter_flight', Record1['flight'])
+prepare_hotel_response = prepare_hotel_transaction('peter_hotel', Record1['hotel'])
+prepare_account_response = prepare_account_transaction('peter_account', Record1['account'])
 
 # Commit transactions
+commit_flight_response = finish_prepared_flight('peter_flight', 'commit')
+commit_hotel_response = finish_prepared_hotel('peter_hotel', 'commit')
+commit_account_response = finish_prepared_account('peter_account', 'commit')
 
-fly, hotel, account = commit_prepared_transaction('peter_flight', 'peter_hotel', 'peter_account')
+# Rollback transactions
+rollback_flight_response = finish_prepared_flight('peter_flight', 'rollback')
+rollback_hotel_response = finish_prepared_hotel('peter_hotel', 'rollback')
+rollback_account_response = finish_prepared_account('peter_account', 'rollback')
+
+
+
 
 
 # Test data
